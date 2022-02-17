1cf6c4a4a9c43c881133fbb8582ed4db3ff167a2
h.yoshino <osushicrusher@gmail.com>
2022-02-18 01:47:33 +0900

    Text component

---
+++ src/libs/Text.tsx
@@ -0,0 +1,17 @@
+import { VFC } from 'react'
+import styled from 'styled-components'
+import { fontSize } from './constants'
+
+const Wrapper = styled.p`
+    font-size: ${fontSize.m}
+`
+
+// 型エイリアスを使ってpropsの型を宣言する
+type Props = {
+    text: string
+    className?: string
+}
+
+export const Text: VFC<Props> = ({ text }) => {
+    return <Wrapper className={className}>{ text }</Wrapper>
+}
